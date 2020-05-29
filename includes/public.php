9d133b5edfc881acd3693b96b2a967592462629f
Fahid Javid <fahidjavid@gmail.com>
2020-05-29 20:30:37 +0500

    Moved the facebook libraries to the “includes” directory.

---
+++ includes/public.php
@@ -0,0 +1,33 @@
+<?php
+
+require_once get_template_directory() . '/common/partials/vendor/autoload.php';
+$fb = new Facebook\Facebook([
+	'app_id' => 'todo_app_id',
+	'app_secret' => 'todo_api_secret',
+	'default_graph_version' => 'v2.10',
+	'default_access_token' => 'access_token'
+	]);
+  
+  try {
+	// Returns a `Facebook\Response` object
+	$response = $fb->get('/me?fields=id,email,name,first_name,last_name,picture');
+  } catch(Facebook\Exception\ResponseException $e) {
+	echo 'Graph returned an error: ' . $e->getMessage();
+	exit;
+  } catch(Facebook\Exception\SDKException $e) {
+	echo 'Facebook SDK returned an error: ' . $e->getMessage();
+	exit;
+  }
+  
+  $user = $response->getGraphUser();
+  ?>
+  <pre>
+  <?php
+// echo ($user['first_name']) . '<br>';
+// echo ($user['last_name']) . '<br>';
+// echo ($user['email']) . '<br>';
+// echo ($user['picture']['url']) . '<br><br><br><br>';
+print_r($user); 
+ ?>
+  </pre>
+  <?php
