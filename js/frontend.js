1687e93675fc95a4cb40b27f995490829e83b250
Fahid Javid <fahidjavid@gmail.com>
2020-05-30 17:10:20 +0500

    Added ajax request JS script.

--- js/frontend.js
+++ js/frontend.js
@@ -1,7 +1,33 @@
 (function($){
 	
 	$('.rsl-provider').on('click', function(e){
-		console.log($( this ).data('provider'));
+
+		var $provider = $( this ).data('provider');
+
+		console.log('rsl_'+ $provider +'_oauth_url');
+
+        $.ajax({
+            type: 'POST',
+            url: rsl_data.ajax_url,
+            dataType: 'json',
+            data: {
+                'action' : 'rsl_'+ $provider +'_oauth_url'
+            },
+            beforeSend: function( ) {
+                console.log('sending...');
+            },
+            complete: function(){
+                console.log('completed!');
+            },
+            success: function (response) { 
+               if(response.success){
+				 window.location.replace(response.oauth_url);
+			   }
+            },
+            error: function(error) {
+                console.log(error);
+            }
+        });
 	});
 
 })(jQuery);
