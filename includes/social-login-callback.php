7c9c2097fbb07d53059e7154b297b70e064184c4
Fahid Javid <fahidjavid@gmail.com>
2020-06-01 14:28:48 +0500

    Added FB profile information fetcher.

--- includes/social-login-callback.php
+++ includes/social-login-callback.php
@@ -4,10 +4,6 @@ if ( ( isset( $_GET['code'] ) && isset( $_GET['state'] ) ) ) {
 	rsl_facebook_login( $_GET );
 } elseif ( isset( $_GET['code'] ) ) {
 	rsl_google_login();
-} else {
-	if ( ! is_user_logged_in() ) {
-		wp_safe_redirect( home_url() );
-	}
 }
 
 function rsl_facebook_login() {
@@ -15,7 +11,7 @@ function rsl_facebook_login() {
 		session_start();
 	}
 	// TODO: ensure FB class is already loaded and no need to load here.
-	require_once __DIR__ . '/../facebook/autoload.php';
+	// require_once __DIR__ . '/../facebook/autoload.php';
 
 	$fb_app_keys = rsl_facebook_app_keys();
 	$fb_args     = array(
@@ -71,7 +67,7 @@ function rsl_facebook_login() {
 
 	try {
 		// Returns a `Facebook\Response` object.
-		$response = $fb->get( '/me?fields=id,email,name,first_name,last_name,picture' );
+		$response = $fb->get( '/me?fields=id,email,name' );
 	} catch ( Facebook\Exception\ResponseException $e ) {
 		echo 'Graph returned an error: ' . esc_html( $e->getMessage() );
 		exit;
@@ -81,4 +77,34 @@ function rsl_facebook_login() {
 	}
 
 	$user = $response->getGraphUser();
+
+	//TODO: first check if user is not registered already with the user email.
+
+	$email         = $user['email'];
+	$username      = explode( '@', $email );
+	$username      = $username[0];
+	$display_name  = $user['name'];
+	$profile_image = 'https://graph.facebook.com/' . $user['id'] . '/picture?width=300&height=300';
+	$password      = $user['id'];
+
+	rsl_social_register( $email, $username, $display_name, $password, $profile_image );
+
+	// $creds                  = array();
+	// $creds['user_login']    = $username;
+	// $creds['user_password'] = $password;
+	// $creds['remember']      = true;
+	// $user_signon            = wp_signon( $creds, false );
+
+	// if ( is_wp_error( $user_signon ) ) {
+	// 	wp_safe_redirect( home_url() );
+	// 	exit;
+	// } else {
+	// 	// TODO: get the user members page dynamically.
+	// 	wp_safe_redirect( 'https://rh.o/edit-profile/' );
+	// 	exit;
+	// }
+}
+
+function rsl_social_register( $email, $username, $display_name, $password, $profile_image ) {
+	var_dump( $email, $username, $display_name, $password, $profile_image );
 }
