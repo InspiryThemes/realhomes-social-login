d127f7e4e70fc81c6bfea16b1eb0990749f3e910
Fahid Javid <fahidjavid@gmail.com>
2020-06-02 17:37:16 +0500

    Added ‘rsl_google_oauth_url’ ajax call handler.

--- includes/social-login-handler.php
+++ includes/social-login-handler.php
@@ -24,12 +24,12 @@ if ( ! function_exists( 'rsl_facebook_oauth_url' ) ) {
 			$helper = $fb->getRedirectLoginHelper();
 
 			$permissions = array( 'public_profile', 'email' ); // App permissions.
-			$login_url   = $helper->getLoginUrl( get_home_url( null, '/', 'https' ), $permissions );
+			$oauth_url   = $helper->getLoginUrl( get_home_url( null, '/', 'https' ), $permissions );
 
 			echo wp_json_encode(
 				array(
 					'success'   => true,
-					'oauth_url' => $login_url,
+					'oauth_url' => $oauth_url,
 					'message'   => esc_html__( 'Redirecting you to facebook for the authentication...', 'realhomes-social-login' ),
 				)
 			);
@@ -48,3 +48,54 @@ if ( ! function_exists( 'rsl_facebook_oauth_url' ) ) {
 
 add_action( 'wp_ajax_nopriv_rsl_facebook_oauth_url', 'rsl_facebook_oauth_url' );
 add_action( 'wp_ajax_rsl_facebook_oauth_url', 'rsl_facebook_oauth_url' );
+
+if ( ! function_exists( 'rsl_google_oauth_url' ) ) {
+	/**
+	 * Return the facebook login authorization url.
+	 */
+	function rsl_google_oauth_url() {
+
+		if ( class_exists( 'Google_Client' ) && class_exists( 'Google_Oauth2Service' ) && null !== rsl_google_app_creds() ) {
+
+			$google_app_creds     = rsl_google_app_creds();
+			$google_client_id     = $google_app_creds['client_id'];
+			$google_client_secret = $google_app_creds['client_secret'];
+			$google_developer_key = $google_app_creds['developer_key'];
+			// $google_redirect_url  = home_url();
+			$google_redirect_url  = 'https://fahid.inspiryzone.com/realhomes/';
+
+			$client = new Google_Client();
+
+			$client->setApplicationName( 'Login to RealHomes' );
+			$client->setClientId( $google_client_id );
+			$client->setClientSecret( $google_client_secret );
+			$client->setDeveloperKey( $google_developer_key );
+			$client->setRedirectUri( $google_redirect_url );
+			$client->setScopes( array( 'email', 'profile' ) );
+
+			// $google_oauthV2 = new Google_Oauth2Service($client);
+			$oauth_url = $client->createAuthUrl();
+
+			echo wp_json_encode(
+				array(
+					'success'   => true,
+					'oauth_url' => $oauth_url,
+					'message'   => esc_html__( 'Redirecting you to google for the authentication...', 'realhomes-social-login' ),
+				)
+			);
+
+		} else {
+			echo wp_json_encode(
+				array(
+					'success' => false,
+					'message' => esc_html__( 'Google library is not loaded.', 'realhomes-social-login' ),
+				)
+			);
+		}
+
+		wp_die();
+	}
+}
+
+add_action( 'wp_ajax_nopriv_rsl_google_oauth_url', 'rsl_google_oauth_url' );
+add_action( 'wp_ajax_rsl_google_oauth_url', 'rsl_google_oauth_url' );
