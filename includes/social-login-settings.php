cdbafdea69ce04fa1d0873ff2721c7d09f5644d5
Fahid Javid <fahidjavid@gmail.com>
2020-06-05 20:51:46 +0500

    Added settings field basic markup and register settings function.

--- includes/social-login-settings.php
+++ includes/social-login-settings.php
@@ -21,6 +21,10 @@ if ( ! class_exists( 'Realhomes_Social_Login_Settings' ) ) {
 	 */
 	class Realhomes_Social_Login_Settings {
 
+		public function __construct() {
+			
+		}
+
 		/**
 		 * Add plugin settings page menu to the dashboard settings menu.
 		 *
@@ -40,5 +44,60 @@ if ( ! class_exists( 'Realhomes_Social_Login_Settings' ) ) {
 
 		}
 
+		/**
+		 * Render settings on the settings page.
+		 *
+		 * @since  1.0.0
+		 */
+		public function render_settings_page() {
+
+			$rsl_settings = get_option( 'rsl_settings' );
+
+			?>
+			<div class="wrap">
+				<h2><?php esc_html_e( 'RealHomes Social Login Settings', 'realhomes-social-login' ); ?></h2>
+				<form method="post" action="options.php">
+
+					<?php settings_fields( 'rsl_settings_group' ); ?>
+					<table class="form-table">
+						<tbody>
+							<!-- Social Login enable disable -->
+							<tr valign="top">
+								<th scope="row" valign="top">
+									<?php esc_html_e( 'Social Login', 'realhomes-social-login' ); ?>
+								</th>
+								<td>
+									<?php
+										$enable_social_login = ! empty( $rsl_settings['enable_social_login'] ) ? $rsl_settings['enable_social_login'] : '';
+									?>
+									<input id="rsl_settings[enable_social_login]" name="rsl_settings[enable_social_login]" type="checkbox" value="1" <?php checked( 1, $enable_social_login ); ?> />
+									<label class="description" for="rsl_settings[enable_social_login]"><?php esc_html_e( 'Enable Social Login on Login forms.', 'realhomes-social-login' ); ?></label>
+								</td>
+							</tr>
+
+						</tbody>
+					</table>
+
+					<p class="submit">
+						<input type="submit" class="button-primary" value="<?php esc_html_e( 'Save Options', 'realhomes-social-login' ); ?>"/>
+					</p>
+
+				</form>
+			</div>
+			<?php
+		}
+
+		/**
+		 * Register settings for the plugin.
+		 *
+		 * @since  1.0.0
+		 */
+		public function register_settings() {
+			register_setting( 'rsl_settings_group', 'rsl_settings' );
+		}
+
 	}
+
+	// Initiate the social login settings class.
+	new Realhomes_Social_Login_Settings();
 }
