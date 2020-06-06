ad9619c14a7df178198c1921a30768b9d4121195
Fahid Javid <fahidjavid@gmail.com>
2020-06-06 14:14:49 +0500

    Moved social login handler file to public and applied apps keys from settings.

--- public/social-login-handler.php
+++ public/social-login-handler.php
@@ -2,7 +2,8 @@
 /**
  * This file is responsible to handle all plugin ajax requests.
  *
- * @package realhomes-social-login
+ * @package    realhomes-social-login
+ * @subpackage realhomes-social-login/public
  */
 
 if ( ! function_exists( 'rsl_facebook_oauth_url' ) ) {
@@ -15,15 +16,17 @@ if ( ! function_exists( 'rsl_facebook_oauth_url' ) ) {
 		require_once RSL_PLUGIN_DIR . 'includes/libs/facebook/autoload.php';
 
 		// TODO: separate these errors of library and keys.
-		if ( class_exists( 'Facebook\Facebook' ) && null !== rsl_facebook_app_keys() ) {
+		if ( class_exists( 'Facebook\Facebook' ) && null !== rsl_app_keys( 'facebook' ) ) {
 
-			$fb_app_keys = rsl_facebook_app_keys();
+			$fb_app_keys = rsl_app_keys( 'facebook' );
 
-			$fb = new Facebook\Facebook([
-				'app_id'                => esc_html( $fb_app_keys['app_id'] ),
-				'app_secret'            => esc_html( $fb_app_keys['app_secret'] ),
-				'default_graph_version' => 'v2.10',
-			]);
+			$fb = new Facebook\Facebook(
+				array(
+					'app_id'                => esc_html( $fb_app_keys['app_id'] ),
+					'app_secret'            => esc_html( $fb_app_keys['app_secret'] ),
+					'default_graph_version' => 'v2.10',
+				)
+			);
 
 			$helper = $fb->getRedirectLoginHelper();
 
@@ -63,12 +66,12 @@ if ( ! function_exists( 'rsl_google_oauth_url' ) ) {
 		require_once RSL_PLUGIN_DIR . 'includes/libs/google/Google_Client.php';
 		require_once RSL_PLUGIN_DIR . 'includes/libs/google/contrib/Google_Oauth2Service.php';
 
-		if ( class_exists( 'Google_Client' ) && class_exists( 'Google_Oauth2Service' ) && null !== rsl_google_app_creds() ) {
+		if ( class_exists( 'Google_Client' ) && class_exists( 'Google_Oauth2Service' ) && null !== rsl_app_keys( 'google' ) ) {
 
-			$google_app_creds     = rsl_google_app_creds();
+			$google_app_creds     = rsl_app_keys( 'google' );
 			$google_client_id     = $google_app_creds['client_id'];
 			$google_client_secret = $google_app_creds['client_secret'];
-			$google_developer_key = $google_app_creds['developer_key'];
+			$google_developer_key = $google_app_creds['api_key'];
 			$google_redirect_url  = home_url();
 
 			$client = new Google_Client();
@@ -116,12 +119,12 @@ if ( ! function_exists( 'rsl_twitter_oauth_url' ) ) {
 		// Twitter library.
 		require_once RSL_PLUGIN_DIR . 'includes/libs/twitter/autoload.php';
 
-		if ( class_exists( 'Abraham\TwitterOAuth\TwitterOAuth' ) ) {
-			session_start();
+		if ( class_exists( 'Abraham\TwitterOAuth\TwitterOAuth' ) && null !== rsl_app_keys( 'twitter' ) ) {
 
-			$consumer_key    = get_option( 'rsl_twitter_app_consumer_key' );
-			$consumer_secret = get_option( 'rsl_twitter_app_consumer_secret' );
-			$callback_url    = home_url( '/' );
+			$twitter_app_keys = rsl_app_keys( 'twitter' );
+			$consumer_key     = $twitter_app_keys['consumer_key'];
+			$consumer_secret  = $twitter_app_keys['consumer_secret'];
+			$callback_url     = home_url( '/' );
 
 			$connection    = new Abraham\TwitterOAuth\TwitterOAuth( $consumer_key, $consumer_secret );
 			$request_token = $connection->oauth( 'oauth/request_token', array( 'oauth_callback' => $callback_url ) );
