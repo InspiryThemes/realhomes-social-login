5bb306b6370a90d5fbf464876714a96d2e8271f4
Fahid Javid <fahidjavid@gmail.com>
2020-06-06 20:04:29 +0500

    Refactored the callbacks functions.

--- admin/social-login-callback.php
+++ admin/social-login-callback.php
@@ -6,14 +6,13 @@
  * @subpackage realhomes-social-login/admin
  */
 
-if ( ( isset( $_GET['code'] ) && isset( $_GET['state'] ) ) ) {
+if ( rsl_is_enabled( 'facebook' ) && ( isset( $_GET['code'] ) && isset( $_GET['state'] ) ) ) {
 	add_action( 'init', 'rsl_facebook_oauth_login' );
-} elseif ( isset( $_GET['code'] ) ) {
+} elseif ( rsl_is_enabled( 'google' ) && isset( $_GET['code'] ) ) {
 	add_action( 'init', 'rsl_google_oauth_login' );
-} elseif ( isset( $_GET['oauth_token'] ) && isset( $_GET['oauth_verifier'] ) ) {
+} elseif ( rsl_is_enabled( 'twitter' ) && isset( $_GET['oauth_token'] ) && isset( $_GET['oauth_verifier'] ) ) {
 	add_action( 'init', 'rsl_twitter_oauth_login' );
 }
-
 
 if ( ! function_exists( 'rsl_facebook_oauth_login' ) ) {
 	/**
@@ -24,141 +23,80 @@ if ( ! function_exists( 'rsl_facebook_oauth_login' ) ) {
 		// Facebook library.
 		require_once RSL_PLUGIN_DIR . 'includes/libs/facebook/autoload.php';
 
-		$fb_app_keys = rsl_facebook_app_keys();
+		if ( class_exists( 'Facebook\Facebook' ) && null !== rsl_app_keys( 'facebook' ) ) {
 
-		if ( class_exists( 'Facebook\Facebook' ) )
-
-		if ( null === $fb_app_keys ) {
-			return;
-		}
-
-		$fb_args = array(
-			'app_id'                => $fb_app_keys['app_id'],
-			'app_secret'            => $fb_app_keys['app_secret'],
-			'default_graph_version' => 'v2.10',
-		);
-
-		$fb = new Facebook\Facebook( $fb_args );
-
-		$helper = $fb->getRedirectLoginHelper();
-
-		if ( isset( $_GET['state'] ) ) {
-			$helper->getPersistentDataHandler()->set( 'state', $_GET['state'] );
-		}
-
-		try {
-			$access_token_obj = $helper->getAccessToken();
-		} catch ( Facebook\Exception\ResponseException $e ) {
-			// When Graph returns an error.
-			echo 'Graph returned an error: ' . esc_html( $e->getMessage() );
-			exit;
-		} catch ( Facebook\Exception\SDKException $e ) {
-			// When validation fails or other local issues.
-			echo 'Facebook SDK returned an error: ' . esc_html( $e->getMessage() );
-			exit;
-		}
-
-		if ( ! isset( $access_token_obj ) ) {
-			if ( $helper->getError() ) {
-				header( 'HTTP/1.0 401 Unauthorized' );
-				echo 'Error: ' . esc_html( $helper->getError() ) . '\n';
-				echo 'Error Code: ' . esc_html( $helper->getErrorCode() ) . '\n';
-				echo 'Error Reason: ' . esc_html( $helper->getErrorReason() ) . '\n';
-				echo 'Error Description: ' . esc_html( $helper->getErrorDescription() ) . '\n';
-			} else {
-				header( 'HTTP/1.0 400 Bad Request' );
-				echo 'Bad request';
-			}
-			exit;
-		}
-
-		$access_token = (string) $access_token_obj->getValue();
-
-		$fb = new Facebook\Facebook(
-			array(
-				'app_id'                => esc_html( $fb_app_keys['app_id'] ),
-				'app_secret'            => esc_html( $fb_app_keys['app_secret'] ),
+			$fb_app_keys = rsl_app_keys( 'facebook' );
+			$fb_args     = array(
+				'app_id'                => $fb_app_keys['app_id'],
+				'app_secret'            => $fb_app_keys['app_secret'],
 				'default_graph_version' => 'v2.10',
-				'default_access_token'  => $access_token,
-			)
-		);
+			);
 
-		try {
-			// Returns a `Facebook\Response` object.
-			$response = $fb->get( '/me?fields=id,email,name,first_name,last_name' );
-		} catch ( Facebook\Exception\ResponseException $e ) {
-			echo 'Graph returned an error: ' . esc_html( $e->getMessage() );
-			exit;
-		} catch ( Facebook\Exception\SDKException $e ) {
-			echo 'Facebook SDK returned an error: ' . esc_html( $e->getMessage() );
-			exit;
-		}
+			$fb = new Facebook\Facebook( $fb_args );
 
-		$user = $response->getGraphUser();
+			$helper = $fb->getRedirectLoginHelper();
 
-		$register_cred['user_email']    = $user['email'];
-		$register_cred['user_login']    = explode( '@', $user['email'] );
-		$register_cred['user_login']    = $register_cred['user_login'][0];
-		$register_cred['display_name']  = $user['name'];
-		$register_cred['first_name']    = $user['first_name'];
-		$register_cred['last_name']     = $user['last_name'];
-		$register_cred['profile_image'] = 'https://graph.facebook.com/' . $user['id'] . '/picture?width=300&height=300';
-		$register_cred['user_pass']     = $user['id'];
+			if ( isset( $_GET['state'] ) ) {
+				$helper->getPersistentDataHandler()->set( 'state', $_GET['state'] );
+			}
 
-		$user_registered = rsl_social_register( $register_cred );
+			try {
+				$access_token_obj = $helper->getAccessToken();
+			} catch ( Facebook\Exception\ResponseException $e ) {
+				// When Graph returns an error.
+				echo 'Graph returned an error: ' . esc_html( $e->getMessage() );
+				exit;
+			} catch ( Facebook\Exception\SDKException $e ) {
+				// When validation fails or other local issues.
+				echo 'Facebook SDK returned an error: ' . esc_html( $e->getMessage() );
+				exit;
+			}
 
-		if ( $user_registered ) {
+			if ( ! isset( $access_token_obj ) ) {
+				if ( $helper->getError() ) {
+					header( 'HTTP/1.0 401 Unauthorized' );
+					echo 'Error: ' . esc_html( $helper->getError() ) . '\n';
+					echo 'Error Code: ' . esc_html( $helper->getErrorCode() ) . '\n';
+					echo 'Error Reason: ' . esc_html( $helper->getErrorReason() ) . '\n';
+					echo 'Error Description: ' . esc_html( $helper->getErrorDescription() ) . '\n';
+				} else {
+					header( 'HTTP/1.0 400 Bad Request' );
+					echo 'Bad request';
+				}
+				exit;
+			}
 
-			$login_creds                  = array();
-			$login_creds['user_login']    = $register_cred['user_login'];
-			$login_creds['user_password'] = $register_cred['user_pass'];
-			$login_creds['remember']      = true;
+			$access_token = (string) $access_token_obj->getValue();
 
-			rsl_social_login( $login_creds );
-		}
+			$fb = new Facebook\Facebook(
+				array(
+					'app_id'                => esc_html( $fb_app_keys['app_id'] ),
+					'app_secret'            => esc_html( $fb_app_keys['app_secret'] ),
+					'default_graph_version' => 'v2.10',
+					'default_access_token'  => $access_token,
+				)
+			);
 
-	}
-}
+			try {
+				// Returns a `Facebook\Response` object.
+				$response = $fb->get( '/me?fields=id,email,name,first_name,last_name' );
+			} catch ( Facebook\Exception\ResponseException $e ) {
+				echo 'Graph returned an error: ' . esc_html( $e->getMessage() );
+				exit;
+			} catch ( Facebook\Exception\SDKException $e ) {
+				echo 'Facebook SDK returned an error: ' . esc_html( $e->getMessage() );
+				exit;
+			}
 
-if ( ! function_exists( 'rsl_google_oauth_login' ) ) {
-	/**
-	 * Google oauth login.
-	 */
-	function rsl_google_oauth_login() {
-
-		// Google Client and Oauth libraries.
-		require_once RSL_PLUGIN_DIR . 'includes/libs/google/Google_Client.php';
-		require_once RSL_PLUGIN_DIR . 'includes/libs/google/contrib/Google_Oauth2Service.php';
-
-		$google_app_creds     = rsl_google_app_creds();
-		$google_client_id     = $google_app_creds['client_id'];
-		$google_client_secret = $google_app_creds['client_secret'];
-		$google_developer_key = $google_app_creds['developer_key'];
-		$google_redirect_url  = home_url();
-
-		$google_client = new Google_Client();
-		$google_client->setApplicationName( 'Login to' . get_bloginfo( 'name' ) );
-		$google_client->setClientId( $google_client_id );
-		$google_client->setClientSecret( $google_client_secret );
-		$google_client->setDeveloperKey( $google_developer_key );
-		$google_client->setRedirectUri( $google_redirect_url );
-		$google_client->setScopes( array( 'email', 'profile' ) );
-
-		$google_oauth_v2 = new Google_Oauth2Service( $google_client );
-		$code            = sanitize_text_field( wp_unslash( $_GET['code'] ) );
-		$google_client->authenticate( $code );
-
-		if ( $google_client->getAccessToken() ) {
-
-			$user = $google_oauth_v2->userinfo->get();
+			$user = $response->getGraphUser();
 
 			$register_cred['user_email']    = $user['email'];
 			$register_cred['user_login']    = explode( '@', $user['email'] );
 			$register_cred['user_login']    = $register_cred['user_login'][0];
 			$register_cred['display_name']  = $user['name'];
-			$register_cred['first_name']    = isset( $user['given_name'] ) ? $user['given_name'] : '';
-			$register_cred['last_name']     = isset( $user['family_name'] ) ? $user['family_name'] : '';
-			$register_cred['profile_image'] = $user['picture'];
+			$register_cred['first_name']    = $user['first_name'];
+			$register_cred['last_name']     = $user['last_name'];
+			$register_cred['profile_image'] = 'https://graph.facebook.com/' . $user['id'] . '/picture?width=300&height=300';
 			$register_cred['user_pass']     = $user['id'];
 
 			$user_registered = rsl_social_register( $register_cred );
@@ -176,6 +114,65 @@ if ( ! function_exists( 'rsl_google_oauth_login' ) ) {
 	}
 }
 
+if ( ! function_exists( 'rsl_google_oauth_login' ) ) {
+	/**
+	 * Google oauth login.
+	 */
+	function rsl_google_oauth_login() {
+
+		// Google Client and Oauth libraries.
+		require_once RSL_PLUGIN_DIR . 'includes/libs/google/Google_Client.php';
+		require_once RSL_PLUGIN_DIR . 'includes/libs/google/contrib/Google_Oauth2Service.php';
+
+		if ( class_exists( 'Google_Client' ) && class_exists( 'Google_Oauth2Service' ) && null !== rsl_app_keys( 'google' ) ) {
+
+			$google_app_creds     = rsl_app_keys( 'google' );
+			$google_client_id     = $google_app_creds['client_id'];
+			$google_client_secret = $google_app_creds['client_secret'];
+			$google_developer_key = $google_app_creds['developer_key'];
+			$google_redirect_url  = home_url();
+
+			$google_client = new Google_Client();
+			$google_client->setApplicationName( 'Login to' . get_bloginfo( 'name' ) );
+			$google_client->setClientId( $google_client_id );
+			$google_client->setClientSecret( $google_client_secret );
+			$google_client->setDeveloperKey( $google_developer_key );
+			$google_client->setRedirectUri( $google_redirect_url );
+			$google_client->setScopes( array( 'email', 'profile' ) );
+
+			$google_oauth_v2 = new Google_Oauth2Service( $google_client );
+			$code            = sanitize_text_field( wp_unslash( $_GET['code'] ) );
+			$google_client->authenticate( $code );
+
+			if ( $google_client->getAccessToken() ) {
+
+				$user = $google_oauth_v2->userinfo->get();
+
+				$register_cred['user_email']    = $user['email'];
+				$register_cred['user_login']    = explode( '@', $user['email'] );
+				$register_cred['user_login']    = $register_cred['user_login'][0];
+				$register_cred['display_name']  = $user['name'];
+				$register_cred['first_name']    = isset( $user['given_name'] ) ? $user['given_name'] : '';
+				$register_cred['last_name']     = isset( $user['family_name'] ) ? $user['family_name'] : '';
+				$register_cred['profile_image'] = $user['picture'];
+				$register_cred['user_pass']     = $user['id'];
+
+				$user_registered = rsl_social_register( $register_cred );
+
+				if ( $user_registered ) {
+
+					$login_creds                  = array();
+					$login_creds['user_login']    = $register_cred['user_login'];
+					$login_creds['user_password'] = $register_cred['user_pass'];
+					$login_creds['remember']      = true;
+
+					rsl_social_login( $login_creds );
+				}
+			}
+		}
+	}
+}
+
 if ( ! function_exists( 'rsl_twitter_oauth_login' ) ) {
 	/**
 	 * Twitter oauth login.
@@ -185,35 +182,39 @@ if ( ! function_exists( 'rsl_twitter_oauth_login' ) ) {
 		// Twitter library.
 		require_once RSL_PLUGIN_DIR . 'includes/libs/twitter/autoload.php';
 
-		$consumer_key    = get_option( 'rsl_twitter_app_consumer_key' );
-		$consumer_secret = get_option( 'rsl_twitter_app_consumer_secret' );
+		if ( class_exists( 'Abraham\TwitterOAuth\TwitterOAuth' ) && null !== rsl_app_keys( 'twitter' ) ) {
 
-		$connection    = new Abraham\TwitterOAuth\TwitterOAuth( $consumer_key, $consumer_secret );
-		$request_token = $connection->oauth( 'oauth/access_token', array( 'oauth_consumer_key' => $consumer_key, 'oauth_token' => $_GET['oauth_token'], 'oauth_verifier' => $_GET['oauth_verifier'] ) );
+			$twitter_app_keys = rsl_app_keys( 'twitter' );
+			$consumer_key     = $twitter_app_keys['consumer_key'];
+			$consumer_secret  = $twitter_app_keys['consumer_secret'];
 
-		$connection = new Abraham\TwitterOAuth\TwitterOAuth( $consumer_key, $consumer_secret, $request_token['oauth_token'], $request_token['oauth_token_secret'] );
-		$user       = (array) $connection->get( 'account/verify_credentials', array( 'include_email' => 'true' ) );
+			$connection    = new Abraham\TwitterOAuth\TwitterOAuth( $consumer_key, $consumer_secret );
+			$request_token = $connection->oauth( 'oauth/access_token', array( 'oauth_consumer_key' => $consumer_key, 'oauth_token' => $_GET['oauth_token'], 'oauth_verifier' => $_GET['oauth_verifier'] ) );
 
-		$register_cred['user_email']    = $user['email'];
-		$register_cred['user_login']    = explode( '@', $user['email'] );
-		$register_cred['user_login']    = $register_cred['user_login'][0];
-		$register_cred['display_name']  = $user['name'];
-		$register_cred['first_name']    = explode( ' ', $user['name'] );
-		$register_cred['first_name']    = $register_cred['first_name'][0];
-		$register_cred['last_name']     = isset( $user['first_name'][1] ) ? $user['first_name'][1] : '';
-		$register_cred['profile_image'] = str_replace( '_normal', '_400x400', $user['profile_image_url_https'] );
-		$register_cred['user_pass']     = $user['id'];
+			$connection = new Abraham\TwitterOAuth\TwitterOAuth( $consumer_key, $consumer_secret, $request_token['oauth_token'], $request_token['oauth_token_secret'] );
+			$user       = (array) $connection->get( 'account/verify_credentials', array( 'include_email' => 'true' ) );
 
-		$user_registered = rsl_social_register( $register_cred );
+			$register_cred['user_email']    = $user['email'];
+			$register_cred['user_login']    = explode( '@', $user['email'] );
+			$register_cred['user_login']    = $register_cred['user_login'][0];
+			$register_cred['display_name']  = $user['name'];
+			$register_cred['first_name']    = explode( ' ', $user['name'] );
+			$register_cred['first_name']    = $register_cred['first_name'][0];
+			$register_cred['last_name']     = isset( $user['first_name'][1] ) ? $user['first_name'][1] : '';
+			$register_cred['profile_image'] = str_replace( '_normal', '_400x400', $user['profile_image_url_https'] );
+			$register_cred['user_pass']     = $user['id'];
 
-		if ( $user_registered ) {
+			$user_registered = rsl_social_register( $register_cred );
 
-			$login_creds                  = array();
-			$login_creds['user_login']    = $register_cred['user_login'];
-			$login_creds['user_password'] = $register_cred['user_pass'];
-			$login_creds['remember']      = true;
+			if ( $user_registered ) {
 
-			rsl_social_login( $login_creds );
+				$login_creds                  = array();
+				$login_creds['user_login']    = $register_cred['user_login'];
+				$login_creds['user_password'] = $register_cred['user_pass'];
+				$login_creds['remember']      = true;
+
+				rsl_social_login( $login_creds );
+			}
 		}
 	}
 }
